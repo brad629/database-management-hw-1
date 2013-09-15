deace06dce15cb1f2e767b5b1a0922eef384b7d9
brad629 <bradley.huntington1@marist.edu>
2013-09-15 19:41:19 -0400

    Update hw-1.sql

--- hw-1.sql
+++ hw-1.sql
@@ -1,4 +1,32 @@
 database-management-hw-1
 ========================
+--Brad Huntington
+--home work 1
+--9/15/13
 
-home work
+--Question 1.
+
+Select *
+From Customers;
+
+
+--Question 2.
+Select
+Agents.name, Agents.city
+
+From
+Agents
+
+Where
+Agents.name='Smith';
+
+
+--Question 3.
+Select
+Products.pid, Products.name, Products.quantity 
+
+From 
+Products
+
+Where
+Products.priceusd > 1.25
