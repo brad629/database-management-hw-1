b3d0d1fa549fe3a140614b348d673f3916bfbadd
brad629 <bradley.huntington1@marist.edu>
2013-09-15 20:18:05 -0400

    Update hw-1.sql

--- hw-1.sql
+++ hw-1.sql
@@ -5,7 +5,6 @@ database-management-hw-1
 --9/15/13
 
 --Question 1.
-
 Select *
 From Customers;
 
@@ -30,3 +29,77 @@ Products
 
 Where
 Products.priceusd > 1.25
+
+
+--Question 4.
+Select 
+Orders.ordno, Orders.aid 
+
+From 
+Orders
+
+
+--Question 5.
+Select 
+Customers.name, Customers.city
+
+From 
+Customers
+
+Where
+Customers.city != 'Dallas'
+
+
+--Question 6.
+Select 
+Agents.name
+
+From 
+Agents
+
+Where
+Agents.city = 'New York' Or Agents.city = 'Newark'
+
+
+--Question 7.
+Select *
+
+From 
+Products
+
+Where
+Products.city != 'New york'
+ And
+ Products.city != 'Newark' 
+ And 
+ Products.priceusd <= 1.00
+
+--Question 8.
+Select *
+
+From 
+Orders
+
+Where
+Orders.mon = 'mar' Or Orders.mon = 'jan'
+
+
+--Question 9.
+Select *
+
+From 
+Orders
+
+Where
+Orders.mon = 'feb' And Orders.dollars < 100.00
+-- there are no orders less than 100$ in the month of febuary.
+
+--Question 10.
+Select *
+
+From 
+Orders
+
+Where
+Orders.cid = 'c005'
+-- the customer c005 does not exist in orders. 
