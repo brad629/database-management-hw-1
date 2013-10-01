0d9142aec05fb15bd6e3db0ad007603084d87851
brad629 <bradley.huntington1@marist.edu>
2013-10-01 12:58:39 -0400

    Update DBM HW - 3.sql

--- DBM HW - 3.sql
+++ DBM HW - 3.sql
@@ -4,3 +4,62 @@
 
 
 --Question 1.
+Select 
+agents.city
+
+From
+Agents
+
+Where
+aid in 
+	(select
+	Orders.aid
+	From 
+	Orders
+	Where
+	orders.cid ='c002')
+
+--Question 2.
+Select 
+A.city
+
+From
+Agents A, orders O
+
+Where
+A.aid = O.aid
+And
+O.cid = 'c002'
+
+--Question 3.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
